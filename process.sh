397e7e843d1301c9185853889503235116fd073a
Johnathan Martin <mrjohnathanmartin@gmail.com>
2016-10-01 20:33:37 -0400

    Fix Menu Errors

--- process.sh
+++ process.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-# Charge credit or debit card via stripe 
-#Johnathan Martin 2016
-
-APIKEY='sk_live' #APIKEY FROM STRIPE
+# Charge credit card via strope API
+APIKEY='sk_live_'
 PS3='Please enter your choice: '
-options=("View Balance" "Create Token" "Create Charge" "Quit")
+options=("View Account Balance" "Create Charge Token" "Create Charge" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
@@ -12,6 +10,7 @@ do
             curl https://api.stripe.com/v1/balance \
    -u $APIKEY:
             ;;
+
         "Create Charge Token")
 echo "Card Number"        
   read CARD
@@ -19,30 +18,37 @@ echo "exp month"
 	read expmonth
 echo "exp year"
 	read expyear
+#echo "CVC"
+#	read cvc
+#echo "Zip Code"
+#	read zip
 	
            curl https://api.stripe.com/v1/tokens \
    -u $APIKEY: \
    -d card[number]=$CARD \
    -d card[exp_month]=$expmonth \
    -d card[exp_year]=$expyear 
+ #  -d card[cvc]=$cvc \
+#   -d card[address_zip]=$zip
             ;;
+
         "Create Charge")
             echo "Token"
 	    read token
 	echo "Amount"
 	read AMT
-#            echo "Charge Description"
-#            read description
 curl https://api.stripe.com/v1/charges \
    -u $APIKEY: \
    -d amount=$AMT \
    -d currency=usd \
    -d source=$token \
-   -d description="Business Name"
+   -d description="FitTech"
             ;;
+
         "Quit")
             break
             ;;
+
         *) echo invalid option;;
     esac
 done
